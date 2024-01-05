a71938702ae9f81472dd8d26236a36be6ac5930a
Vanessa Trinh <xtourmaline@gmail.com>
2024-01-05 02:28:52 -0500

    added schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,33 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE Category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR NOT NULL
+);
+
+CREATE TABLE Product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR NOT NULL,
+    price DECIMAL(15, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT(10),
+    category_id INT,
+    FOREIGN KEY (category_id)
+    REFERENCES Category(id)
+);
+
+CREATE TABLE Tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR
+);
+
+CREATE TABLE ProductTag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id)
+    REFERENCES Product(id),
+    FOREIGN KEY (tag_id)
+    REFERENCES Tag(id)
+);
+
