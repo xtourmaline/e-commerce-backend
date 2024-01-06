3ee1cc594c7ef4965816d190013deab6faa2b374
Vanessa Trinh <xtourmaline@gmail.com>
2024-01-06 03:44:59 -0500

    completed tag.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,6 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-CREATE TABLE Product (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR NOT NULL,
-    price DECIMAL(15, 2) NOT NULL,
-    stock INT NOT NULL DEFAULT(10),
-    category_id INT,
-    FOREIGN KEY (category_id)
-    REFERENCES Category(id)
-);
-
-CREATE TABLE Tag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR
-);
-
 CREATE TABLE ProductTag (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      auto_increment: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
