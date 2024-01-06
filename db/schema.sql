fd445bd57294eced751db8ef8925fe94822e5bf6
Vanessa Trinh <xtourmaline@gmail.com>
2024-01-06 04:18:23 -0500

    completed producttag.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-CREATE TABLE ProductTag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_id INT,
-    tag_id INT,
-    FOREIGN KEY (product_id)
-    REFERENCES Product(id),
-    FOREIGN KEY (tag_id)
-    REFERENCES Tag(id)
-);
-
